db511727661a20754d18e1887a30c2086d5c844f
Ilja Panic <ilja@iljapanic.com>
2024-11-03 22:44:20 +0100

    remove nextjs revalidation consts

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { homepageFeaturedCollectionsQuery } from '@/lib/sanity/query'
 import { Homepage, Collections } from '@/lib/sanity/sanity.types'
 import { CollectionsListing } from '@/components/collections/collections-listing'
 
-export const revalidate = 0
-export const dynamic = 'force-dynamic'
+// export const revalidate = 0
+// export const dynamic = 'force-dynamic'
 
 export default async function HomePage() {
 	const homepage = await sanityFetch<
